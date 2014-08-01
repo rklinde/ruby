4dbca0f88fc65ea60e34261fd6b2d61191673cd4
Rebecca Linde <rklinde@gmail.com>
2014-08-01 11:40:06 -0400

    more practice exercises

--- exercises.rb
+++ exercises.rb
@@ -33,10 +33,33 @@ times15(9)
 
 # Performs a mathematical operation using four integer and/or float arguments
 
-
-
 # Prints the argument given four times 
 
-
-
 # Prints an uppercase version of the argument given
+
+
+# # Take two strings and turns them into the key and value of a hash, i.e. make_hash(“hello”, “world”) >{hello: “world”}
+string1 = "hello"
+string2 = "world"
+
+def new_hash(key, value)
+	new_hash = {string1 => string2}
+end
+p new_hash
+
+#Takes a first and last name and returns a full name 
+
+def findName(key, value)
+full_name = {first: "Rebecca", last: "Linde"}
+
+p full_name[:first] + " " + full_name[:last]
+
+#Generates an <img> tag in HTML given an src as a string (“tiger.jpeg”)
+src = "tiger.jpeg"
+p "<img src='" + src + "'/>"
+
+# An A+ is above a 97, an A above a 94, and an A- above a 90. 
+# A B+ is above an 87, a B is above an 84, and a B- is above an 80. 
+# Everything else is an F. 
+# Given a grade numerically (90, 86, 40), return a letter grade (A+, B-, F).
+# Takes a string as an argument and removes any characters that are repeated
--- test.rb
+++ test.rb
@@ -34,6 +34,12 @@ months = {jan: "January", feb: "February"}
 months[:jan]
 # --- returns "January"
 
+#hash practice
+date_results = {height: "6.1", eyes: "brown", good_at_conversation: "true"}
+# -- to get data
+p date_results[:height]
+
+
 #conditionals
 animal = "Zebra"
 
@@ -59,25 +65,38 @@ building == "90 john" && building == "100 john"
 
 
 #loops
+# --- used to locate/do something to a variable, 
+# --- when the variable changes each time the loop iterates
+
+#for loop
 for number in 0..10
+	# for a number within the range of 0-10
 	p number
 end
 # --- returns values 0-10
 
+
+#while loop
 some_number = 0
 #sets counter up
-
 while some_number < 5  
 # until this condition is no longer true 
-	
 	puts some_number   
 	#executes code inside this block
-	
 	some_number = some_number + 1 
 	#sets value of variable to iterate
 end
 
+#each loop
+some_array = ["teddy bear", "stuffed giraffe"]
+# --- declares variable
+some_array.each do |xxx|
+	# --- anything placed within the vertical bar is an arbitary alias to define the values in the array
+	puts xxx
+end
+
 #functions
+# --- the last line in the function is the information that gets returned
 def addTwo(n)
 	#sets name of function
 	n + 2
@@ -87,6 +106,39 @@ end
 a = addTwo(7)
 puts a
 # --- returns 9
+
+#objects
+class User
+#declares the variable "user" as an object
+	attr_accessor :fname
+	attr_accessor :lname
+	# --- attr_accessor assigns the object with an attribute
+	def full_name
+	# --- assigns a method to the object; in this case, how to get the "full name" of a User object
+		fname + " " + lname
+	end
+end
+
+first_user = User.new
+first_user.fname = "Rebecca"
+first_user.lname = "Linde"
+p first_user
+p first_user.full_name
+
+class Burger
+	attr_accessor :temp
+	attr_accessor :topping
+	attr_accessor :condiment
+	def order
+		"I would like my burger #{temp}, with #{topping} and #{condiment}."
+	end
+end
+
+rl_order = Burger.new
+rl_order.temp = "medium rare"
+rl_order.topping = "lettuce"
+rl_order.condiment = "ketchup"
+p rl_order.order
 
 
 
